f2e2d8482edac6ccc3476d8260071175fdafe421
heyangjiedd <1059320200@qq.com>
2019-05-07 23:08:00 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="container">
+    <div>
+      <div v-for="item in posts" :key="item.id">
+        <span>{{item}}</span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import axios from 'axios'
+
+export default {
+  components: {
+    Logo
+  },
+  async asyncData() {
+    const posts = await axios.get('http://visney.cn:81/NavigationMeta/get/12');
+    const posts1 =  await axios.get('https://jsonplaceholder.typicode.com/posts')
+    return {posts: Object.keys(posts.data)}
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
